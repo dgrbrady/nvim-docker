40d66bf4321dbc4be8e4f209b788cb1d2cbfba04
David Brady <dbrady@stratsupport.com>
2022-07-20 16:12:00 -0400

    attempt to fix issue where popup doesn't open a 2nd time

--- lua/nvim-docker/containers.lua
+++ lua/nvim-docker/containers.lua
@@ -7,47 +7,55 @@ local Job = require('plenary.job')
 
 local _M = {}
 
-local function render_tree(container_data, p)
+local popup_top_text = 'Docker Containers'
+local popup_bottom_text = '<l>: Expand, <L>: Expand All, <h>: Collapse, <H>: Collapse All, <u>: Container UP, <d>: Container DOWN, <q>: Quit'
+
+local function render_tree(container_data)
     if container_data == nil then
         return
     end
-    local old_nodes = state.tree:get_nodes()
-    print(vim.pretty_print(p))
-    tree.create_tree(p.winid)
-    for index, value in ipairs(container_data) do
-        if value ~= nil then
-            local container = vim.json.decode(value)
-            local text = ''
-            if string.find(container.status, 'Up') then
-                text = container.name .. ' 🟢'
-            else
-                text = container.name .. ' 🔴'
-            end
+    
+    if state.popup == nil then
+        popup.create_popup(popup_top_text, popup_bottom_text, function (p)
+            local old_nodes = state.tree:get_nodes()
+            print(vim.pretty_print(p))
+            tree.create_tree(p.winid)
+            for index, value in ipairs(container_data) do
+                if value ~= nil then
+                    local container = vim.json.decode(value)
+                    local text = ''
+                    if string.find(container.status, 'Up') then
+                        text = container.name .. ' 🟢'
+                    else
+                        text = container.name .. ' 🔴'
+                    end
 
-            local node = NuiTree.Node({ text = text, container = container }, {
-                NuiTree.Node({ text = '[ID] ' .. container.id }),
-                NuiTree.Node({ text = '[Image] ' .. container.image }),
-                NuiTree.Node({ text = '[Command] ' .. container.command }),
-                NuiTree.Node({ text = '[Status] ' .. container.status }),
-                NuiTree.Node({ text = '[Networks] ' .. container.networks }),
-                NuiTree.Node({ text = '[Ports] ' .. container.ports }),
-            })
-            state.tree:add_node(node)
+                    local node = NuiTree.Node({ text = text, container = container }, {
+                        NuiTree.Node({ text = '[ID] ' .. container.id }),
+                        NuiTree.Node({ text = '[Image] ' .. container.image }),
+                        NuiTree.Node({ text = '[Command] ' .. container.command }),
+                        NuiTree.Node({ text = '[Status] ' .. container.status }),
+                        NuiTree.Node({ text = '[Networks] ' .. container.networks }),
+                        NuiTree.Node({ text = '[Ports] ' .. container.ports }),
+                    })
+                    state.tree:add_node(node)
 
-            -- if the node was expanded before it was cleared, expand it again
-            for index, old_node in ipairs(old_nodes) do
-                if old_node.container.id == node.container.id then
-                    if old_node:is_expanded() then
-                        node:expand()
+                    -- if the node was expanded before it was cleared, expand it again
+                    for index, old_node in ipairs(old_nodes) do
+                        if old_node.container.id == node.container.id then
+                            if old_node:is_expanded() then
+                                node:expand()
+                            end
+                        end
                     end
                 end
             end
-        end
+            state.tree:render()
+        end)
     end
-    state.tree:render()
 end
 
-local function get_containers(p)
+local function get_containers()
     local result = Job:new({
         command = 'docker',
         args = {
@@ -58,26 +66,21 @@ local function get_containers(p)
         },
     }):sync()
     if result ~= nil then
-        render_tree(result, p)
+        render_tree(result)
     end
 end
 
 function _M.list_containers()
     if state.popup == nil then
-        popup.create_popup(
-            'Docker Containers',
-            '<l>: Expand, <L>: Expand All, <h>: Collapse, <H>: Collapse All, <u>: Container UP, <d>: Container DOWN, <q>: Quit',
-            function (p)
-                get_containers(p)
-            end
-        )
+        popup.create_popup(popup_top_text, popup_bottom_text, function ()
+            get_containers()
+        end)
     end
 
     -- background refresh the tree every 5000ms
-    state.timer:start(1000, 5000, vim.schedule_wrap(function ()
+    state.timer:start(5000, 5000, vim.schedule_wrap(function ()
         if state.timer_stopped == false then
-            local p = state.popup
-            get_containers(p)
+            get_containers()
         end
     end))
 end
